eed1987d62a9c6b1730393d671b4877400fd81c4
Akhila Dison <100663325+Akhila-Dison@users.noreply.github.com>
2022-03-01 15:00:54 +0530

    Delete 3.Count the occurrence of each word in a line of text.py

--- CO1/3.Count the occurrence of each word in a line of text.py
+++
@@ -1,7 +0,0 @@
-k="In its most general sense, the term world refers to the totality of entities, to the whole of reality or to everything that is"
-h=[]
-for i in k.split(" "):
-	if i not in h:
-		h.append(i)
-for i in h:
-	print(i,":",k.split(" ").count(i))
